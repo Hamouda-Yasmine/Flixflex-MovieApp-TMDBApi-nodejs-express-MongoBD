5588937291811b323d505b31f63d3b954e3b807d
Hamouda-Yasmine <hamouda.yasmine9@gmail.com>
2023-11-23 21:47:38 +0100

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,110 @@
-# Flixflex-MovieApp-TMDBApi-nodejs-express-MongoBD
+
+
+
+
+
+
+# FlexFlix Movie App Backend
+
+This is the backend application for the Movie App project. It provides endpoints to interact with movie and TV show data, user authentication, and user favorites.
+
+## Table of Contents
+- [FlexFlix Movie App Backend](#flexflix-movie-app-backend)
+  - [Table of Contents](#table-of-contents)
+  - [Getting Started](#getting-started)
+    - [Prerequisites](#prerequisites)
+    - [Installation](#installation)
+  - [Endpoints](#endpoints)
+  - [User Authentication](#user-authentication)
+  - [Favorites](#favorites)
+  - [Search](#search)
+  - [Movie and TV Show Details](#movie-and-tv-show-details)
+  - [Pagination](#pagination)
+  - [Top Movies and TV Shows](#top-movies-and-tv-shows)
+  - [Error Handling](#error-handling)
+  - [Contributing](#contributing)
+ 
+
+## Getting Started
+
+### Prerequisites
+- Node.js
+- MongoDB Atlas account (for database)
+
+### Installation
+1. Clone the repository.
+2. Install dependencies using `npm install`.
+3. Set up a MongoDB Atlas cluster and update the connection string in `connectBD.ts`.
+4. Obtain a TMDB API key and update the `apiKey` variable in `index.ts`.
+
+## Endpoints
+
+- `POST /signup`: User registration.
+- `GET /movies`: Get a list of movies.
+- `GET /tv`: Get a list of TV shows.
+- `GET /movies/top`: Get the top 5 most rated movies.
+- `GET /tv/top`: Get the top 5 most rated TV shows.
+- `GET /moviesPagination`: Get a paginated list of movies.
+- `GET /tvPagination`: Get a paginated list of TV shows.
+- `POST /add-favorite`: Add a movie or TV show to the user's favorites.
+- `DELETE /delete-favorite`: Remove a movie or TV show from the user's favorites.
+- `GET /favorites/:userId`: Get the list of user's favorite movies and TV shows.
+- `GET /search/movie`: Search for movies based on a query.
+- `GET /moviedetail/:id`: Get details of a specific movie.
+- `GET /tv/:id`: Get details of a specific TV show.
+
+## User Authentication
+
+- Endpoint: `POST /signup`
+  - Create a new user account.
+
+## Favorites
+
+- Endpoint: `POST /add-favorite`
+  - Add a movie or TV show to the user's favorites.
+
+- Endpoint: `DELETE /delete-favorite`
+  - Remove a movie or TV show from the user's favorites.
+
+- Endpoint: `GET /favorites/:userId`
+  - Get the list of user's favorite movies and TV shows.
+
+## Search
+
+- Endpoint: `GET /search/movie`
+  - Search for movies based on a query.
+
+## Movie and TV Show Details
+
+- Endpoint: `GET /moviedetail/:id`
+  - Get details of a specific movie.
+
+- Endpoint: `GET /tv/:id`
+  - Get details of a specific TV show.
+
+## Pagination
+
+- Endpoint: `GET /moviesPagination`
+  - Get a paginated list of movies.
+
+- Endpoint: `GET /tvPagination`
+  - Get a paginated list of TV shows.
+
+## Top Movies and TV Shows
+
+- Endpoint: `GET /movies/top`
+  - Get the top 5 most rated movies.
+
+- Endpoint: `GET /tv/top`
+  - Get the top 5 most rated TV shows.
+
+## Error Handling
+- 200 OK: Successful request.
+- 404 Not Found: When a requested resource is not found.
+- 500 Internal Server Error: For unexpected server errors.
+
+## Contributing
+
+Feel free to contribute to this project. Create a pull request with your changes.
+
+
--- dist/index.js
+++ dist/index.js
@@ -15,13 +15,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const axios_1 = __importDefault(require("axios"));
 const User_1 = __importDefault(require("./User"));
-const body_parser_1 = __importDefault(require("body-parser"));
 const connectBD_1 = __importDefault(require("./connectBD"));
 const app = (0, express_1.default)();
-const PORT = 5003;
+const PORT = 5002;
 const apiKey = "64d2030684a562afa048c30263e478c8";
 //middlware pour parser le corps des requetes en json
-app.use(body_parser_1.default.json());
+app.use(express_1.default.json());
 //Connect to MongoDB Atlas
 (0, connectBD_1.default)();
 // Signup endpoint
@@ -106,17 +105,18 @@ app.get('/moviesPagination', (req, res) => __awaiter(void 0, void 0, void 0, fun
         const pageNumber = parseInt(req.query.page) || 1;
         const size = 10;
         const response = yield axios_1.default.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en`);
-        const totalResults = response.data.total_results;
-        const totalPages = response.data.total_pages;
-        // Fetch movies for the specified page and size
+        const nombretotalmovies = response.data.total_results;
+        // supprimer la virgule
+        const totalPages = Math.floor(nombretotalmovies / 10);
+        // recuperer la position (debut,fin) des films rn fonction de la page et size
         const startIdx = (pageNumber - 1) * size;
         const endIdx = startIdx + size;
-        const allMovies = response.data.results.slice(startIdx, endIdx);
+        const tenMovies = response.data.results.slice(startIdx, endIdx);
         res.status(200).json({
-            totalResults,
+            nombretotalmovies,
             totalPages,
             currentPage: pageNumber,
-            movies: allMovies,
+            movies: tenMovies,
         });
     }
     catch (error) {
@@ -124,9 +124,134 @@ app.get('/moviesPagination', (req, res) => __awaiter(void 0, void 0, void 0, fun
         res.status(500).json({ message: 'Internal Server Error' });
     }
 }));
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+//getting the list of 10 tv on a specific page number 
+app.get('/tvPagination', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const pageNumber = parseInt(req.query.page) || 1;
+        const size = 10;
+        const response = yield axios_1.default.get(`https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}`);
+        const nombretotaltv = response.data.total_results;
+        console.log("nombre total", nombretotaltv);
+        // supprimer la virgule
+        const totalPages = Math.floor(nombretotaltv / 10);
+        // recuperer la position (debut,fin) des films rn fonction de la page et size
+        const startIdx = (pageNumber - 1) * size;
+        const endIdx = startIdx + size;
+        const tenMovies = response.data.results.slice(startIdx, endIdx);
+        res.status(200).json({
+            nombretotaltv,
+            totalPages,
+            currentPage: pageNumber,
+            movies: tenMovies,
+        });
+    }
+    catch (error) {
+        console.error('Error fetching movies:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}));
+// add movies or tv in my favorite list
+app.post('/add-favorite', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        //655cf25c6a9d6abb10d3a103
+        const userId = req.body.userId;
+        //670292
+        const movieId = req.body.movieId;
+        // Find the user by ID
+        const user = yield User_1.default.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        // Add the movie ID to favorites if not already in the list
+        if (!user.favorites.includes(movieId)) {
+            user.favorites.push(movieId);
+            yield user.save();
+            return res.status(200).json({ message: 'Movie added to favorites successfully' });
+        }
+        else {
+            return res.status(400).json({ message: 'Movie already in favorites' });
+        }
+    }
+    catch (error) {
+        console.error('Error adding movie to favorites:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}));
+// delete from list of favorites
+app.delete('/delete-favorite', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const userId = req.body.userId;
+        const movieId = req.body.movieId;
+        const user = yield User_1.default.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        user.favorites = user.favorites.filter(id => id !== movieId);
+        yield user.save();
+        return res.status(200).json({ message: 'Movie deleted from favorites successfully' });
+    }
+    catch (error) {
+        console.error('Error deleting movie from favorites:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}));
+// to view the list of my favorite movies and series
+app.get('/favorites/:userId', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const userId = req.params.userId;
+        const user = yield User_1.default.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const favoriteIDs = user.favorites || [];
+        return res.status(200).json(favoriteIDs);
+    }
+    catch (error) {
+        console.error('Error retrieving favorite movies:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}));
+// search fo movies
+app.get('/search/movie', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { query } = req.query;
+    try {
+        const response = yield axios_1.default.get('https://api.themoviedb.org/3/search/movie', {
+            params: {
+                api_key: apiKey,
+                query: query,
+            },
+        });
+        res.json(response.data.results);
+    }
+    catch (error) {
+        console.error('Error searching for movie:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}));
+//consulter le detail d'un film 
+app.get('/moviedetail/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const idmovie = req.params.id;
+    try {
+        const response = yield axios_1.default.get(`https://api.themoviedb.org/3/movie/${idmovie}?api_key=${apiKey}&language=en`);
+        res.status(200).json(response.data);
+    }
+    catch (error) {
+        console.error(`Error fetching movie details for ID ${idmovie}:`, error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}));
+//consulter le detail d'une serie 
+app.get('/tv/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const idtv = req.params.id;
+    try {
+        const response = yield axios_1.default.get(`https://api.themoviedb.org/3/tv/${idtv}?api_key=${apiKey}`);
+        res.status(200).json(response.data);
+    }
+    catch (error) {
+        console.error(`Error fetching tv details for ID ${idtv}:`, error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}));
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
